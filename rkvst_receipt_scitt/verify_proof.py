e56e3c8ef6b9ff606d5f2dbce8db25146cfed67d
jgough <joe.gough@rkvst.com>
2023-02-28 11:07:38 +0000

    Attribute decode, Problem: need to rlp decode our attributes, Solution: add library to decode all our rkvst attribute types, signed-off-by: joe.gough@rkvst.com

---
+++ rkvst_receipt_scitt/verify_proof.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+""" Module for verifying all the proofs given in a scitt receipt"""
+
+def verifiy_receipt():
+    """
+    verifies the scitt receipt
+    """
+    return None
